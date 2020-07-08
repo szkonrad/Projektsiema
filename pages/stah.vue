ba39dd10af7259ed1212408baf7ef22360870081
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-08 23:42:57 +0200

    es?

--- pages/stah.vue
+++ pages/stah.vue
@@ -3,145 +3,122 @@
 
 
 <div class="container">
-<div class="stickybar"><center>kiedys tu dam przyciski kozak co nie?</center> </div>
+<div class="stickybar">
+    <ul>
+        <li><a> XD      </a></li>
+        <li><a> XD      </a></li>
+        <li><a> XD      </a></li>
+
+    </ul>
+ </div>
 
     <div class="jeszcze nic">
-        XD</br>
-                XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
-        XD</br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+        XD<br>
+
+
 
 
 
@@ -156,35 +133,6 @@
 
 
 <style>
-
-html,
-body
-{
-	min-width: 500px;
-	background-position: center;
-	background-attachment: fixed;
-	-webkit-background-size: cover;
-	-moz-background-size: cover;
-	-o-background-size: cover;
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-image: url("https://i.imgur.com/36PjIIx.jpg");
-	min-height: 100%;
-	min-width: 100%;
-	color: gray;
-	background-color: #1E1E1E;
-	margin: 0;
-	padding: 0;	
-}
-
-.stickybar{
-    min-width: 100%;
-   
-    position: fixed;
-    background-color: black;
-
-}
-
-
+@import url('..\Pliki CSS\stah\styles.css');
 </style>
 
