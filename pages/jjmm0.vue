e29f3c46a9197c367b51a1ba11fac17a9471f44f
jjmm0 <jedrzej.miga@gmail.com>
2020-07-08 23:35:36 +0200

    moja stronka

--- pages/jjmm0.vue
+++ pages/jjmm0.vue
@@ -1,6 +1,10 @@
 <template>
+
     <header>
-        Witam ogółem na mojej stronie
+        <h1>Witam ogółem na mojej stronie </h1>
     </header>
   
 </template>
+<style>
+@import url('..\Pliki CSS\jjmm0_CSS\jjmm0css.css');
+</style>
