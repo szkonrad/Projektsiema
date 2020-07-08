d5b0026da37517fae06ace35f1b71b7655ef936b
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-08 21:30:52 +0200

    j00r
    suchodolski

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
           rel="noopener noreferrer"
           class="button--green"
         >
-          Documentation
+         ooo Documentation o sciane poleci
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
@@ -21,9 +21,50 @@
           rel="noopener noreferrer"
           class="button--grey"
         >
-          GitHub
+          Ooo github
         </a>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
