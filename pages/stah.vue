ad9afd0e683e2eb83a0b2224b9489ab4e1149530
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-08 22:34:18 +0200

    Update stah.vue

--- pages/stah.vue
+++ pages/stah.vue
@@ -1,11 +1,38 @@
 <template>
-    <div id="container">
-        <div> elo tu jednak nie githab D: </div>
-DJSDAAJSDJASLKDJSD
+<div class="container">
+    <div class="topbar">
+        elo tu jednak nie githab D: tylko stah
+
 
     </div>
+</div>
     
 
 
 </template>
 
+<style>
+
+body{
+
+background-color: gray;
+
+}
+
+
+.topbar{
+    
+    color: white;
+    background-color: black;
+    min-height: 80%;
+    min-width: 80%;
+    margin-top: 5%;
+    margin-left: 5%;
+    margin-right: 5%;
+
+
+
+}
+
+
+</style>
