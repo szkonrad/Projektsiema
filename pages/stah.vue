a524f646e7c0b08928fb5e533addcb0176a21d2e
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-08 23:18:52 +0200

    XD

--- pages/stah.vue
+++ pages/stah.vue
@@ -7,29 +7,8 @@
 
     <div class="jeszcze nic">
         XD
-        XDxd</br>
-        xd</br>
-        xd
-                XD
-        XDxd</br>
-        xd</br>
-        xd
-                XD
-        XDxd</br>
-        xd</br>
-        xd
-                XD
-        XDxd</br>
-        xd</br>
-        xd
-                XD
-        XDxd</br>
-        xd</br>
-        xd
-                XD
-        XDxd</br>
-        xd</br>
-        xd
+
+
 
     </div>
 </div>
@@ -42,32 +21,11 @@
 
 <style>
 
-html,
-body
-{
-	min-width: 500px;
-	background-position: center;
-	background-attachment: fixed;
-	-webkit-background-size: cover;
-	-moz-background-size: cover;
-	-o-background-size: cover;
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-image: url("https://i.imgur.com/36PjIIx.jpg");
-	min-height: 100%;
-	min-width: 100%;
-	color: gray;
-	background-color: #1E1E1E;
-	margin: 0;
-	padding: 0;	
-}
+  @import './Pliki_CSS/stah/styles.css';
 
-.stickybar{
 
-    position: fixed;
-    background-color: black;
 
-}
+
 
 
 
