9cc80f470b3e0013daf2a22f7cbe196108184557
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-08 23:26:33 +0200

    Update stah.vue

--- pages/stah.vue
+++ pages/stah.vue
@@ -3,10 +3,146 @@
 
 
 <div class="container">
-<div class="stickybar">kiedys tu dam przyciski kozak co nie? </div>
+<div class="stickybar"><center>kiedys tu dam przyciski kozak co nie?</center> </div>
 
     <div class="jeszcze nic">
-        XD
+        XD</br>
+                XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+        XD</br>
+
 
 
 
@@ -21,12 +157,33 @@
 
 <style>
 
-  @import './Pliki_CSS/stah/styles.css';
+html,
+body
+{
+	min-width: 500px;
+	background-position: center;
+	background-attachment: fixed;
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-image: url("https://i.imgur.com/36PjIIx.jpg");
+	min-height: 100%;
+	min-width: 100%;
+	color: gray;
+	background-color: #1E1E1E;
+	margin: 0;
+	padding: 0;	
+}
 
+.stickybar{
+    min-width: 100%;
+   
+    position: fixed;
+    background-color: black;
 
-
-
-
+}
 
 
 </style>
