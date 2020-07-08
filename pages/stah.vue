ff4940a0e084f576fae943e4306fd4bcccdb03fb
Komornikx <68018904+Komornikx@users.noreply.github.com>
2020-07-08 23:16:11 +0200

    Update stah.vue

--- pages/stah.vue
+++ pages/stah.vue
@@ -1,10 +1,35 @@
 <template>
 
 
-<div class="container">
-    <div class="container">
-        elo tu jednak nie githab D: tylko stah
 
+<div class="container">
+<div class="stickybar">kiedys tu dam przyciski kozak co nie? </div>
+
+    <div class="jeszcze nic">
+        XD
+        XDxd</br>
+        xd</br>
+        xd
+                XD
+        XDxd</br>
+        xd</br>
+        xd
+                XD
+        XDxd</br>
+        xd</br>
+        xd
+                XD
+        XDxd</br>
+        xd</br>
+        xd
+                XD
+        XDxd</br>
+        xd</br>
+        xd
+                XD
+        XDxd</br>
+        xd</br>
+        xd
 
     </div>
 </div>
@@ -17,26 +42,33 @@
 
 <style>
 
-body{
-
-background-color: gray;
-
+html,
+body
+{
+	min-width: 500px;
+	background-position: center;
+	background-attachment: fixed;
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-image: url("https://i.imgur.com/36PjIIx.jpg");
+	min-height: 100%;
+	min-width: 100%;
+	color: gray;
+	background-color: #1E1E1E;
+	margin: 0;
+	padding: 0;	
 }
 
+.stickybar{
 
-.container{
-    
-    color: white;
+    position: fixed;
     background-color: black;
-    min-height: 80%;
-    min-width: 80%;
-    margin-top: 5%;
-    margin-left: 10%;
-    margin-right: 10%;
-
-
 
 }
+
 
 
 </style>
