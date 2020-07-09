6fa3a558082563f888d5e6cc215eb05af927c41f
jjmm0 <jedrzej.miga@gmail.com>
2020-07-09 22:48:38 +0200

    nic

--- pages/jjmm0.vue
+++ pages/jjmm0.vue
@@ -5,6 +5,7 @@
         <a href="..\jjmm0historia"><span class="naw" id="1">Historia</span></a>
         <a href=".."><span class="naw" id="2">Strona Główna</span></a>
         <a href="..\jjmm0obrazy"><span class="naw" id="3">Obrazy</span></a>
+
         </div>
         </center>
 </div>
--- pages/jjmm0obrazy.vue
+++ pages/jjmm0obrazy.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <div>
+<img src="..\images\jjmm0\6ac3d23b4c736fe781179bb53f627ab1.jpg">
+<img src="..\images\jjmm0\138733.jpg">
+<img src="..\images\jjmm0\paint_liquid_abstract_130239_1920x1080.jpg">
+<img src="..\images\jjmm0\unnamed.jpg">
 
 </div>
     </div>
 </template>
+
+
+<style>
+@import url('..\Pliki CSS\jjmm0_CSS\jjmm0obrazy.css');
+</style>
