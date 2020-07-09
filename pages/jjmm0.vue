d1cf014824870f421539f491641425f95115c9f4
jjmm0 <jedrzej.miga@gmail.com>
2020-07-09 20:33:44 +0200

    .

--- pages/jjmm0.vue
+++ pages/jjmm0.vue
@@ -2,7 +2,7 @@
 <div>
     <center>
     <div class="pasek">
-        <a href="..\jjmm0historia.vue"><span class="naw" id="1">Historia</span></a>
+        <a href="..\jjmm0historia"><span class="naw" id="1">Historia</span></a>
         <a href=".."><span class="naw" id="2">Strona Główna</span></a>
         <span class="naw" id="3">Obrazy</span>
         </div>
